8c3b792295492df395837109fc54af9a3faf68bd
oksana markiv <markiv_o@qwasar.io>
2021-05-13 07:25:35 +0000

    My Robot Simulator

--- ex00/my_robot_simulator.js
+++ ex00/my_robot_simulator.js
@@ -1,37 +1,56 @@
-function my_robot_simulator(instructions){
-    var position = {x: 0, y: 0, bearing: 'north'};
-    for(i=0; i=instructions.length; i++){
-        if(instructions[i] == 'A'){
-            switch(position.bearing){
-                case 'north':
+function my_robot_simulator( instructions ){
+    var position = { x: 0, y: 0, bearing: 'north' };
+
+for( i = 0; i < instructions.length; i++ ){
+
+    if( instructions[i] == 'A' ){
+
+        switch( position.bearing ){
+            case 'north':
                 position.y--;
                 break;
-                case 'south':
+            case 'south':
                 position.y++;
                 break;
-                case 'east':
+            case 'east':
                 position.x++;
                 break;
-                case 'west':
-                position.x--;
-                break;
-            }  
-        } else if(instructions[i] == 'R' || instructions[i] == 'L'){
-        switch (position.bearing){
-            case 'north':
-               position.bearing = (insturctions[i] = 'R') ? 'east' : 'west';
-               break;
-            case 'south':
-               position.bearing = (insturctions[i] = 'R') ? 'west' : 'east';
-               break;
-            case 'east':
-               position.bearing = (insturctions[i] = 'R') ? 'south' : 'north';
-               break;
             case 'west':
-               position.bearing = (insturctions[i] = 'R') ? 'north' : 'south';
-               break; 
+                position.x--;
+                break;            
+        }
+} else if( instructions[i] == 'R' || instructions[i] == 'L' ){
+         switch( position.bearing ){
+            case 'north':
+               // if( instructions[i] == 'R' ){
+               //     position.bearing = 'east'
+               // } else {
+               //     position.bearing = 'west'
+               // }  
+                position.bearing = ( instructions[i] == 'R' ) ? 'east' : 'west';
+                break;
+            case 'south':
+                position.bearing = ( instructions[i] == 'R' ) ? 'west' : 'east';
+
+
+                break;
+            case 'east':
+                position.bearing = ( instructions[i] == 'R' ) ? 'south' : 'north';
+
+                break;
+            case 'west':
+                position.bearing = ( instructions[i] == 'R' ) ? 'north' : 'south';
+
+                break;            
         }
     }
 }
-return`{x: ${position.x}, y: ${position.y}, bearing: '${position.bearing}'}`;
+
+return `{x: ${position.x}, y: ${position.y}, bearing: '${position.bearing}'}`;
+
 }
+
+// console.log(my_robot_simulator("RAALALL"));
+// console.log(my_robot_simulator("AAAA"));
+// console.log(my_robot_simulator("RAARA"));
+// console.log(my_robot_simulator(""));
